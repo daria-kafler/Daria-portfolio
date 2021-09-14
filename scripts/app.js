4e591c76f2027a7ada8a9fdec7b71ddf9d696c71
Daria Kafler <me@dariakafler.com>
2021-09-14 15:00:05 +0100

    menu button now fuctional, added animated menu, lots of styling still to do

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+// function init() {
+//   function openNav() {
+//     document.getElementById('mySidenav').style.width = '100%'
+//   }
+//   function closeNav() {
+//     document.getElementById('mySidenav').style.width = '0'
+//   }
+  
+//   function handleClick(event) {
+//     console.log(event)
+//   }
+
+// }
+// window.addEventListener('DOMContectLoaded', init)
+// window.addEventListener('DOMContentLoaded', (event) => {
+//   console.log(event, 'JS good to go')
+// })
